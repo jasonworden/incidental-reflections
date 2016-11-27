50e3949017bb18281fd00ac6050739a11cf9e11d
Jason Worden <wordenjasonp@gmail.com>
2016-11-26 19:52:53 -0800

    Simplifying movement

--- incidental-reflections-repo.ino
+++ incidental-reflections-repo.ino
@@ -10,10 +10,9 @@ const int STEPS_PER_REV = (STEP == HALFSTEP) ? 2048 : 1024;
 const int MAX_SPEED = (STEP == HALFSTEP) ? 2000 : 1000;
 
 const float REVS = 8;
-long target = (long)( float(STEPS_PER_REV) * REVS );
+long maxTargetDistance = (long)( float(STEPS_PER_REV) * REVS );
 
 const int NUM_STEPPERS = 11;
-long positions[NUM_STEPPERS];
 
 // Give each stepper a name/# which begins at 1 at one of the middle motors
 // and spirals outward towards an outer edge corner of the 4x4 motor grid:
@@ -21,7 +20,7 @@ AccelStepper stepper1(STEP, 47, 51, 49, 53);
 AccelStepper stepper2(STEP, 6, 8, 7, 9);
 AccelStepper stepper3(STEP, 31, 35, 33, 37);
 AccelStepper stepper4(STEP, 13, 11, 12, 10);
-AccelStepper stepper5(STEP, 1, 14, 0, 15);
+AccelStepper stepper5(STEP, 22, 26, 24, 28);
 AccelStepper stepper6(STEP, 30, 34, 32, 36);
 AccelStepper stepper7(STEP, 5, 3, 4, 2);
 AccelStepper stepper8(STEP, 46, 50, 48, 52);
@@ -43,69 +42,49 @@ AccelStepper steppers[NUM_STEPPERS] = {
   stepper11
 };
 
-const int stepperIndicesAtEnds[2] = {0, NUM_STEPPERS-1};
-int stepperIndexAtEndRef = 1;
-int lastStepperToFinishIndex = stepperIndicesAtEnds[stepperIndexAtEndRef]; //NUM_STEPPERS-1
-AccelStepper* lastStepperToFinish = &steppers[lastStepperToFinishIndex];
+bool isForwards = true;
+AccelStepper *stepper;
 
 //////////
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("----- BEGIN SETUP CODE");
-  
-  for(int i=0; i<NUM_STEPPERS; ++i) {
-    steppers[i].setMaxSpeed(MAX_SPEED);
-    steppers[i].setSpeed(MAX_SPEED);
-    steppers[i].setAcceleration(MAX_SPEED*4);
-    positions[i] = calculateIncrementalTargetPosition(target, i, NUM_STEPPERS, true);
-    Serial.println(positions[i], DEC);
-    steppers[i].moveTo(positions[i]);
-  }
 
-  Serial.println("----- END SETUP CODE");
+  for(int i=0; i<NUM_STEPPERS; ++i) {
+    stepper = &steppers[i];
+    stepper->setMaxSpeed(MAX_SPEED);
+    stepper->setAcceleration(MAX_SPEED*4);
+    stepper->setSpeed(MAX_SPEED);
+    stepper->moveTo(99999999);
+  }
 }
 
 void loop()
 {
-  long distance = lastStepperToFinish->distanceToGo();
-  if(DEBUG) {
-    Serial.print("last stepper (index ");
-    Serial.print(lastStepperToFinishIndex);
-    Serial.print("): ");
-    Serial.println((int)distance);
-  }
-  if(distance == 0) {
-    Serial.println("ALL STEPPERS HAVE REACHED TARGET. Reversing now...");
-    
-    target = -target;
-    
-    stepperIndexAtEndRef = (stepperIndexAtEndRef + 1) % 2;
-    lastStepperToFinishIndex = stepperIndicesAtEnds[stepperIndexAtEndRef];
-    lastStepperToFinish = &steppers[lastStepperToFinishIndex];
-    
-    bool isForwards = (lastStepperToFinishIndex != 0);
-    if(!isForwards) {
-      for(int i=NUM_STEPPERS; i>=0; --i) {
-        positions[i] = calculateIncrementalTargetPosition(target, i, NUM_STEPPERS, isForwards);
-        steppers[i].moveTo(positions[i]);
-      }
-    } else {
-      for(int i=0; i<NUM_STEPPERS; ++i) {
-        positions[i] = calculateIncrementalTargetPosition(target, i, NUM_STEPPERS, isForwards);
-        steppers[i].moveTo(positions[i]);
-      }
+    for(int i=0; i<NUM_STEPPERS; ++i) {
+      stepper = &steppers[i];
+      stepper->run();
     }
-    
-  }
 
-  for(int i=0; i<NUM_STEPPERS; ++i) {
-    if(steppers[i].distanceToGo() != 0) {
-      steppers[i].run();
-    }
-  }
-   
+//  if(isForwards) {
+//    for(int i=0; i<NUM_STEPPERS; ++i) {
+//      stepper = &steppers[i];
+//      long target = calculateIncrementalTargetPosition(maxTargetDistance, i, NUM_STEPPERS, isForwards);
+//      stepper->moveTo(target);
+//      stepper->runToPosition();
+//    }
+//  } else {
+//    for(int i=NUM_STEPPERS; i>=0; --i) {
+//      stepper = &steppers[i];
+//      long target = calculateIncrementalTargetPosition(maxTargetDistance, i, NUM_STEPPERS, isForwards);
+//      stepper->moveTo(target);
+//      stepper->runToPosition();
+//    }
+//  }
+//
+//  Serial.println("ALL STEPPERS HAVE REACHED TARGET. Reversing now...");
+//  isForwards = !isForwards;
 }
 
 /////////////////
@@ -115,7 +94,7 @@ void loop()
 long calculateIncrementalTargetPosition(long maxTarget, int i, int totalCount, bool isForwards)
 {
   if(i == totalCount) return maxTarget;
-   
+  if(!isForwards)     maxTarget = -maxTarget;
   float fraction = (float)(i+1) / (float)totalCount;
   float targetPos = (isForwards) ?
     (float)( 0.3 + 0.7*fraction ) * (float)maxTarget :
