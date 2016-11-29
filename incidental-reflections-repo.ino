044251cea821f464bb2d812728adc1889b0da451
Jason Worden <wordenjasonp@gmail.com>
2016-11-29 13:58:23 -0800

    Updated

--- incidental-reflections-repo.ino
+++ incidental-reflections-repo.ino
@@ -5,23 +5,28 @@ const int HALFSTEP = 8;
 const int FULLSTEP = 4;
 const bool DEBUG = false;
 
-const int STEP = HALFSTEP;
-const int STEPS_PER_REV__FULL = 1024;
+const int STEP = FULLSTEP;
+const int STEPS_PER_REV__FULL = 1045;
 const int STEPS_PER_REV = (STEP == HALFSTEP) ? STEPS_PER_REV__FULL*2 : STEPS_PER_REV__FULL;
-const int MAX_SPEED = (STEP == HALFSTEP) ? 2000 : 917;
+const int MAX_SPEED = (STEP == HALFSTEP) ? 1500 : 750;
 
+//How many revolutions do we want? This is arbitrary.
 const float REVS = 8;
-long target = (long)( float(STEPS_PER_REV) * REVS );
+const long targetMagnitude = (long)( float(STEPS_PER_REV) * REVS );
 
-const int NUM_STEPPERS = 11;
-const float NUM_STEPPERS__FLOAT = (float)NUM_STEPPERS;
+const int NUM_STEPPERS_ARRAY = 13;
+const int NUM_STEPPERS_BOARD = 16;
+const float NUM_STEPPERS_ARRAY_FLOAT = (float)NUM_STEPPERS_ARRAY;
+const float NUM_STEPPERS_BOARD_FLOAT = (float)NUM_STEPPERS_BOARD;
+const int NUM_STEPPERS_DIFF = NUM_STEPPERS_BOARD - NUM_STEPPERS_ARRAY;
 
 // Give each stepper a name/# which begins at 1 at one of the middle motors
-// and spirals outward towards an outer edge corner of the 4x4 motor grid:
-AccelStepper stepper1(STEP, 47, 51, 49, 53);
-AccelStepper stepper2(STEP, 6, 8, 7, 9);
-AccelStepper stepper3(STEP, 31, 35, 33, 37);
+// and spirals outward towards an outer edge corner of the 4x4 motor grid
+
+//Steppers 1-4 are controlled as 1 unit as per stepper4's output:
 AccelStepper stepper4(STEP, 13, 11, 12, 10);
+
+//Steppers 5-16 are all individually controlled:
 AccelStepper stepper5(STEP, 22, 26, 24, 28);
 AccelStepper stepper6(STEP, 30, 34, 32, 36);
 AccelStepper stepper7(STEP, 5, 3, 4, 2);
@@ -29,11 +34,13 @@ AccelStepper stepper8(STEP, 46, 50, 48, 52);
 AccelStepper stepper9(STEP, 23, 27, 25, 29);
 AccelStepper stepper10(STEP, 38, 42, 40, 44);
 AccelStepper stepper11(STEP, 39, 43, 41, 45);
-  
-AccelStepper steppers[NUM_STEPPERS] = {
-  stepper1,
-  stepper2,
-  stepper3,
+AccelStepper stepper12(STEP, 39, 43, 41, 45);
+AccelStepper stepper13(STEP, 39, 43, 41, 45);
+AccelStepper stepper14(STEP, 39, 43, 41, 45);
+AccelStepper stepper15(STEP, 39, 43, 41, 45);
+AccelStepper stepper16(STEP, 39, 43, 41, 45);
+
+AccelStepper steppers[NUM_STEPPERS_ARRAY] = {
   stepper4,
   stepper5,
   stepper6,
@@ -41,51 +48,52 @@ AccelStepper steppers[NUM_STEPPERS] = {
   stepper8,
   stepper9,
   stepper10,
-  stepper11
+  stepper11,
+  stepper12,
+  stepper13,
+  stepper14,
+  stepper15,
+  stepper16
 };
 
 bool isForwards = true;
-int lastStepperIndex = NUM_STEPPERS-1;
+int lastStepperIndex = NUM_STEPPERS_ARRAY-1;
 AccelStepper* lastStepper = &steppers[lastStepperIndex];
-AccelStepper* stepper;
-
-float progress;
-float positionFraction;
+AccelStepper* stepper;  //for current stepper when iterating thru array
+float progressToTarget;         //used in steppers array interation
 
 //////////
 
 void setup()
 {
   Serial.begin(9600);
-
-  setTargetForSteppers(target);
+  setTargetForSteppers(targetMagnitude);
 }
 
 void loop()
 {
   long distanceToGo = lastStepper->distanceToGo();
-  if(distanceToGo == 0) {
+  if(distanceToGo == 0)
+  {
     Serial.println("Reversing now...");
     isForwards = !isForwards;
-    lastStepperIndex = isForwards ? NUM_STEPPERS-1 : 0;
-    lastStepper = & steppers[lastStepperIndex];
+    lastStepperIndex = isForwards ? NUM_STEPPERS_ARRAY - 1 : 0;
+    lastStepper = &steppers[lastStepperIndex];
 
-    long newTarget = isForwards ? target : -target;
+    long newTarget = isForwards ? targetMagnitude : -targetMagnitude;
     setTargetForSteppers(newTarget);
   }
 
-  progress = (float)(target - abs(distanceToGo))/(float)target;
-//  Serial.println(progress);
+  progressToTarget = (float)(targetMagnitude - abs(distanceToGo))/(float)targetMagnitude;
   
-  if(isForwards) {
-    for(int i=0; i<NUM_STEPPERS; ++i) {  stepper = &steppers[i];
-      positionFraction = (float)(i+1)/NUM_STEPPERS__FLOAT;
-      if(positionFraction > progress)  stepper->run();
+  if(isForwards)
+  {
+    for(int i=0; i<NUM_STEPPERS_ARRAY; ++i) {  stepper = &steppers[i];
+      if(calculatePositionFraction(i) > progressToTarget)  stepper->run();
     }
   } else {
-    for(int i=NUM_STEPPERS; i>=0; --i) {  stepper = &steppers[i];
-      positionFraction = (float)(i+1)/NUM_STEPPERS__FLOAT;
-      if(positionFraction < progress)  stepper->run();
+    for(int i=NUM_STEPPERS_ARRAY; i>=0; --i) {  stepper = &steppers[i];
+      if(calculatePositionFraction(i) < progressToTarget)  stepper->run();
     }
   }
   
@@ -95,59 +103,17 @@ void loop()
 //HELPER FUNCTIONS
 /////////////////
 
-long calculateIncrementalTargetPosition(long maxTarget, int i, int totalCount, bool isForwards)
-{
-  if(i == totalCount) return maxTarget;
-  if(!isForwards)     maxTarget = -maxTarget;
-  float fraction = (float)(i+1) / (float)totalCount;
-  float targetPos = (isForwards) ?
-    (float)( 0.3 + 0.7*fraction ) * (float)maxTarget :
-    targetPos = (float)( 1.0 - 0.7*fraction ) * (float)maxTarget; //for backwards
-
-  return (long)targetPos;
+float calculatePositionFraction(int steppersArrayIndex) {
+  return (float)(steppersArrayIndex+1+NUM_STEPPERS_DIFF)/NUM_STEPPERS_BOARD_FLOAT;
 }
 
 void setTargetForSteppers(long targetPosition) {
-  for(int i=0; i<NUM_STEPPERS; ++i) {  stepper = &steppers[i];
+  for(int i=0; i<NUM_STEPPERS_ARRAY; ++i) {  stepper = &steppers[i];
     stepper->setCurrentPosition(0);
     stepper->setMaxSpeed(MAX_SPEED);
-    stepper->setAcceleration(MAX_SPEED*4);
-    stepper->setSpeed(MAX_SPEED);
+    stepper->setAcceleration(MAX_SPEED);
     stepper->moveTo(targetPosition);
   }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//OLD CODE:
-
-//  for(int i=0; i<NUM_STEPPERS; ++i) {
-//    // If at the end of travel go to the other end
-//    if (steppers[i].distanceToGo() == 0) {
-//      steppers[i].moveTo(-steppers[i].currentPosition());
-//    }
-//    steppers[i].run();
-//  }
-
-//  if (steppers[0].distanceToGo() == 0)  {
-//    for(int i=0; i<num_steppers; ++i) {
-//      steppers[i].moveTo(-steppers[i].currentPosition());
-//    }
-//  }
-//  for(int i=0; i<num_steppers; ++i) {
-//    steppers[i].run();
-//  }
